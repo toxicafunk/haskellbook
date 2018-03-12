bc82b70450009a8adea8f5135b2166ba4b3d671c
K5008133 <eric.noam@mindcurv.com>
2018-03-12 22:49:53 +0100

    foldBool with map

--- ch9/squarecube.hs
+++ ch9/squarecube.hs
@@ -1,6 +1,8 @@
 -- squarecube.hs
 module SquareCube where
 
+import Data.Bool
+
 mySqr :: [Integer]
 mySqr = [x^2 | x <- [1..5]]
 
@@ -10,3 +12,5 @@ myCube = [y^3 | y <- [1..5]]
 myTuple :: [(Integer, Integer)]
 myTuple = [(x,y) | x <- mySqr, y <- myCube, x < 50, y < 50]
 
+foldBool :: (Num a, Eq a) => a -> [a] -> [a]
+foldBool b = map (\x -> bool (-x) x (x == b))
